be39d48ce0a5bf0082a4feeee4248b2a7ff6623c
rodrigo <rob.developer@protonmail.com>
2021-07-08 23:33:00 -0400

    initial commit

---
+++ exercise1.rb
@@ -0,0 +1,79 @@
+#Desafío - Herencia, polimorfismo y módulos
+#Exercise 1
+
+#refactored code
+class Person
+    attr_reader :first_name, :last_name, :age
+    def initialize(first, last, age)
+        @first_name = first
+        @last_name = last
+        @age = age
+    end
+    def birthday
+        @age += 1
+    end
+end
+
+class Student
+    def talk
+        puts "Aquí es la clase de programación con Ruby?"
+    end
+    def introduce
+        puts "Hola profesor. Mi nombre es #{@first_name} #{@last_name}."
+    end
+end
+
+class Teacher
+    def talk
+    
+    end
+    def introduce
+    
+    end
+end
+
+class Parent
+    def talk
+    
+    end
+    def introduce
+    
+    end
+end
+
+
+
+
+
+=begin
+class Person
+    def initialize(first, last, age, type)
+        @first_name = first
+        @last_name = last
+        @age = age
+        @type = type
+    end
+    def birthday
+        @age += 1
+    end
+    def talk
+        if @type == "Student"
+            puts "Aquí es la clase de programación con Ruby?"
+        elsif @type == "Teacher"
+            puts "Bienvenidos a la clase de programación con Ruby!"
+        elsif @type == "Parent"
+            puts "Aquí es la reunión de apoderados?"
+        end
+    end
+    def introduce
+        if @type == "Student"
+            puts "Hola profesor. Mi nombre es #{@first_name} #{@last_name}."
+        elsif @type == "Teacher"
+            puts "Hola alumnos. Mi nombre es #{@first_name} #{@last_name}."
+        elsif @type == "Parent"
+            puts "Hola. Soy uno de los apoderados. Mi nombre es #{@first_name} #
+        {@last_name}."
+        end
+    end
+end
+=end
